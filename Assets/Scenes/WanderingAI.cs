08ef974b57d8baf3f4fcb9d3cff635b66f8ebef7
thinkabout4x <pave-shherbakov@yandex.ru>
2023-11-23 21:24:40 +0100

    add enemy movement

---
+++ Assets/Scenes/WanderingAI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+    public float sphereRange = 0.75f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,0,speed*Time.deltaTime);
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.SphereCast(ray, sphereRange, out hit)){
+            if (hit.distance < obstacleRange){
+                float angle = Random.Range(-110,110);
+                transform.Rotate(0,angle,0);
+            } 
+        }
+    }
+}

265c9dbf860f129af2e84ff2ef5747cdc7d27854
thinkabout4x <pave-shherbakov@yandex.ru>
2023-11-24 20:12:04 +0100

    add enemy shooting

---
+++ Assets/Scenes/Fireball.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public int damage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed* Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        PlayerCharachter player = other.GetComponent<PlayerCharachter>();
+        if (player != null) {
+            Debug.Log("Player Hit!");
+        }
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scenes/WanderingAI.cs
+++ Assets/Scenes/WanderingAI.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class WanderingAI : MonoBehaviour
 {
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
     public float sphereRange = 0.75f;
+    public float fireballPosMult = 1.5f;
     private bool isAlive;
+    [SerializeField] GameObject fireballPrefab;
+    private GameObject fireball;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +27,14 @@ public class WanderingAI : MonoBehaviour
             RaycastHit hit;
 
             if (Physics.SphereCast(ray, sphereRange, out hit)){
-                if (hit.distance < obstacleRange){
+                GameObject hitObj = hit.transform.gameObject;
+                if (hitObj.GetComponent<PlayerCharachter>()){
+                    if (fireball == null) {
+                        fireball = Instantiate(fireballPrefab) as GameObject;
+                        fireball.transform.position = transform.TransformPoint(Vector3.forward* fireballPosMult);
+                        fireball.transform.rotation = transform.rotation;
+                    }
+                } else if (hit.distance < obstacleRange){
                     float angle = Random.Range(-110,110);
                     transform.Rotate(0,angle,0);
                 } 
